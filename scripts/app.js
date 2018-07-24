7675b010d9c14a0086b615219f1f239120361d40
Nithin Ashok <nithin.a333@gmail.com>
2018-07-24 13:57:14 -0500

    Hangman Game

---
+++ scripts/app.js
@@ -0,0 +1,30 @@
+const puzzleEl = document.querySelector('#puzzle')
+const guessesEl = document.querySelector('#guesses')
+let game
+
+window.addEventListener('keypress', (e) => {
+    const guess = String.fromCharCode(e.charCode)
+    game.makeGuess(guess)
+    render()
+})
+
+const render = () => {
+    puzzleEl.innerHTML = ''
+    guessesEl.textContent = game.statusMessage
+    
+    game.puzzle.split('').forEach((letter) => {
+        const letterEl = document.createElement('span')
+        letterEl.textContent = letter
+        puzzleEl.appendChild(letterEl)
+    })
+}
+
+const startGame = async () => {
+    const puzzle = await getPuzzle('2')
+    game = new Hangman(puzzle, 5)
+    render()
+}
+
+document.querySelector('#reset').addEventListener('click', startGame)
+
+startGame()
